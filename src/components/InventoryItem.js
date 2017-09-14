239fe9991feb41c1055c8c6ca79d11341a05abd0
Evan Carr-Hall <evancarrhall@gmail.com>
2017-09-14 16:50:53 -0400

    tweak item styling

--- src/components/InventoryItem.js
+++ src/components/InventoryItem.js
@@ -6,7 +6,7 @@ class InventoryItem extends React.Component {
         return(
             <li className="item">
                 <p className="itemName">{this.props.item.name}</p>
-                <p className="itemStock">{this.props.item.stock}</p>
+                <p className="itemStock">{"("+this.props.item.stock+")"}</p>
                 <p className="itemPrice">{"$" + this.props.item.price}</p>
             </li>
         );
